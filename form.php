31e260d3e516f3c1760623ae532218b31442c69a
kaique04 <kaiquedossantosmoreira1@gmail.com>
2024-08-07 18:08:18 -0300

    testes

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+
+    $para = "kaiquedossantosmoreira1@gmail.com";
+    $assunto = "Coleta de dados ";
+
+    $corpo = "nome: ".$nome."\n"."E-mail".$email."\n"."Telefone: ".$telefone;
+
+    $cabeca = "From: kaiquemoreira517@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("houve um erro ao enviar o email!");
+    }
+
+?>
+
